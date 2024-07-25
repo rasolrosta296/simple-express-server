b8aa25ca4f25e1b65742060fa28af34acc525df5
rasool <rasolrostapisheh@gmail.com>
2024-07-25 21:04:36 +0330

    Second commit

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,23 @@
-const express = require("express");
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get('/', (req, res) => res.send('Express on Vercel'));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.get('/data', (req, res) => {
+  const filePath = path.join(__dirname, 'db.json');
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Error reading database file');
+    } else {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(data);
+    }
+  });
+});
+
+app.listen(3000, () => console.log('Server ready on port 3000.'));
 
 module.exports = app;
+
